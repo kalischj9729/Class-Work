d87d7296cc5251bb3932abe6b51bb1a08a9ff200
kalischj9729 <kalischj9729@student.faytechcc.edu>
2024-09-16 11:18:42 -0400

    Add files via upload

---
+++ P1LAB1_Kalisch,Jereme.py
@@ -0,0 +1,19 @@
+# Jereme Klaich
+# 9/16/2024
+# P1LAB1
+# Get input from user and display output
+
+
+# Getr first name from user
+first_name = input('Enter your first name: ')
+
+
+
+# Get last name from user
+last_name = input('Enter your last name: ')
+
+# Welcome the user to CTI-110
+
+print('Hello,', first_name, last_name+'!', 'Welcome to CTI-110!')
+
+
